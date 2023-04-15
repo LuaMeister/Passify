9ac0b7f6f549dacba45ffac973a2ecce418d0b1e
Ryan Hollink <ryan.hollink@hotmail.com>
2023-04-15 15:14:35 +0200

    length argument

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 
-use clap::App;
+use std::any::Any;
+
+use clap::{App, Arg};
 use rand::{seq::{SliceRandom, IteratorRandom}};
 
 fn main() {
@@ -7,9 +9,21 @@ fn main() {
         .about("Creates a random password with confiruable parameters in the command line.")
         .version(env!("CARGO_PKG_VERSION"))
         .author(env!("CARGO_PKG_AUTHORS"))
+        .arg(
+            Arg::with_name("length")
+                .short('l')
+                .takes_value(true)
+                .help("The length of the password to be generated.")
+        )
         .get_matches();
 
-    let password = generate_password(10);
+    let password_size = _application
+        .value_of("length")
+        .unwrap_or("10")
+        .parse::<i16>()
+        .unwrap_or(10);
+
+    let password = generate_password(password_size);    
     println!("{}", password);
 }
 
