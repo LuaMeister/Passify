413727ff0da48670e803dea275c288ed870c0525
Ryan Hollink <ryan.hollink@hotmail.com>
2023-04-15 14:36:15 +0200

    password gen function setup

--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,13 @@ fn main() {
     let _application = App::new("Passify")
         .about("Creates a random password with confiruable parameters in the command line.")
         .version(env!("CARGO_PKG_VERSION"))
-        .author(env!("CARGO_PKG_AUTHORS"));
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .get_matches();
+
+    let password = generate_password();
+    println!("{}", password);
+}
+
+fn generate_password() -> String {
+    "Hello, World".to_string()
 }
