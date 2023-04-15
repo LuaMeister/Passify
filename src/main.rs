d5b84ad9d66bf56cd126c8c0c88522b435d08cd4
Ryan Hollink <ryan.hollink@hotmail.com>
2023-04-15 14:31:42 +0200

    created application with clap App

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+
+use clap::App;
+
 fn main() {
-    println!("Hello, world!");
+    let _application = App::new("Passify")
+        .about("Creates a random password with confiruable parameters in the command line.")
+        .version(env!("CARGO_PKG_VERSION"))
+        .author(env!("CARGO_PKG_AUTHORS"));
 }
