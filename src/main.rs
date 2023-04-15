b8d750cfa30f91962deeb0f0686ac40a469383c5
Ryan Hollink <ryan.hollink@hotmail.com>
2023-04-15 15:02:21 +0200

    password generator basics

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 
 use clap::App;
+use rand::{seq::{SliceRandom, IteratorRandom}};
 
 fn main() {
     let _application = App::new("Passify")
@@ -8,10 +9,21 @@ fn main() {
         .author(env!("CARGO_PKG_AUTHORS"))
         .get_matches();
 
-    let password = generate_password();
+    let password = generate_password(10);
     println!("{}", password);
 }
 
-fn generate_password() -> String {
-    "Hello, World".to_string()
+fn generate_password(length: i16) -> String {
+    let mut password = String::new();
+    let mut random = rand::thread_rng();
+
+    let character_sets = vec!["abcdefghijklmnopqrstuvwxyz", "ABCDEFGHIJKLMNOPQRSTUVW"];
+
+    for _ in 0..length {
+        let character_set = character_sets.choose(&mut random);
+        let character = character_set.unwrap().chars().choose(&mut random).unwrap();
+        password.push(character);
+    }
+
+    password
 }
