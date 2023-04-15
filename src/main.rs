cebe3458bedb6a0547666f1ccb473446b510ab8d
Ryan Hollink <ryan.hollink@hotmail.com>
2023-04-15 15:20:23 +0200

    symbols argument

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
 
-use std::any::Any;
-
 use clap::{App, Arg};
 use rand::{seq::{SliceRandom, IteratorRandom}};
 
@@ -15,23 +13,37 @@ fn main() {
                 .takes_value(true)
                 .help("The length of the password to be generated.")
         )
+        .arg(
+            Arg::with_name("symbols")
+                .short('s')
+                .takes_value(false)
+                .help("Whether or not to include symbols in the password.")
+        )
         .get_matches();
 
+    // Parse Arguments
     let password_size = _application
         .value_of("length")
         .unwrap_or("10")
         .parse::<i16>()
         .unwrap_or(10);
 
-    let password = generate_password(password_size);    
+    let symbols = _application.is_present("symbols");
+
+    // Generate password and output it
+    let password = generate_password(password_size, symbols);    
     println!("{}", password);
 }
 
-fn generate_password(length: i16) -> String {
+fn generate_password(length: i16, symbols: bool) -> String {
     let mut password = String::new();
     let mut random = rand::thread_rng();
 
-    let character_sets = vec!["abcdefghijklmnopqrstuvwxyz", "ABCDEFGHIJKLMNOPQRSTUVW", "01234567891"];
+    let mut character_sets = vec!["abcdefghijklmnopqrstuvwxyz", "ABCDEFGHIJKLMNOPQRSTUVW", "01234567891"];
+
+    if symbols {
+        character_sets.push("!@#$%^&*()_+~`|}{[]:;?><,./-=");
+    }
 
     for _ in 0..length {
         let character_set = character_sets.choose(&mut random);
